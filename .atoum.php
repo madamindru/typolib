0e376a8711f77f1c9b33c7cfa16b3de8c3fa1ee9
TheoChevalier <theo.chevalier11@gmail.com>
2015-04-23 02:08:20 +0200

    Enable coverall.io

---
+++ .atoum.php
@@ -0,0 +1,12 @@
+<?php
+//.atoum.php
+
+use \mageekguy\atoum;
+
+$script->addDefaultReport();
+
+$cloverWriter = new atoum\writers\file('./build/logs/clover.xml');
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+
+$runner->addReport($cloverReport);
